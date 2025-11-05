05ad7c00057468098996d3b28c73b7edd3611ece
Hongwei WU <846922737@qq.com>
2025-11-05 22:24:08 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/smart-city-wuhan/', // ← 加这一行
   plugins: [vue()],
   resolve: {
     alias: {
